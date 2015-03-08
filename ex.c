3e965e7b39d62fea320695806133c934d37d381a
lineulimasjc <lineulimasjc@gmail.com>
2015-03-08 02:29:40 -0300

    updating the ex.c file

--- ex.c
+++ ex.c
@@ -2,10 +2,10 @@
 
 main()
 {
-    int x;
+    int x, y;
 
-    printf("Enter a number: ");
-    scanf("%d", &x);
+    printf("Enter two numbers: ");
+    scanf("%d %d", &x, &y);
 
-    printf("\nResult: %.2f\n\n", pow(x, 2));
+    printf("\nResult: %.2f\n\n", pow(x, y));
 }
